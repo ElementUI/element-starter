ea99839b8bc8a1d013040bc3815d31fcade14053
Bob-huang-gdut <3570411064@qq.com>
2017-07-18 19:06:19 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,33 @@
     </el-switch>
     <el-switch v-model="value2" on-color="#13ce66" off-color="#ff4949">
     </el-switch>
+    <div style="margin: 15px 0;"></div>
+    <el-checkbox-group v-model="checkboxGroup1">
+      <el-checkbox-button v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox-button>
+    </el-checkbox-group>
+    <div style="margin: 15px 0;"></div>
+    <el-checkbox-group v-model="checkboxGroup2" size="small">
+      <el-checkbox-button v-for="city in cities" :label="city" :disabled="city === '深圳'" :key="city">{{city}}</el-checkbox-button>
+    </el-checkbox-group>
+    <div style="margin: 15px 0;"></div>
+    <el-checkbox-group v-model="checkboxGroup3" size="large" fill="#324057" text-color="#a4aebd" :min="1" :max="3">
+      <el-checkbox-button v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox-button>
+    </el-checkbox-group>
   </div>
 </template>
 
 <script>
+const cityOptions = ['上海', '北京', '广州', '深圳'];
 export default {
   data() {
     return {
       msg: 'Use Vue 2.0 Today!',
       value1: true,
-      value2: true
+      value2: true,
+      checkboxGroup1: ['上海'],
+      checkboxGroup2: ['北京'],
+      checkboxGroup3: ['广州'],
+      cities: cityOptions
     }
   },
 

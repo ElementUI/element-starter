a6bdee7087714183cc4e90bba99753c8f6a45a18
Bob-huang-gdut <3570411064@qq.com>
2017-07-18 23:29:04 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }} </h1>
     <el-button @click.native="startHacking">Let's do it</el-button>
     <el-switch v-model="value1" on-text="" off-text="">
     </el-switch>
@@ -19,6 +19,8 @@
     <el-checkbox-group v-model="checkboxGroup3" size="large" fill="#324057" text-color="#a4aebd" :min="1" :max="3">
       <el-checkbox-button v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox-button>
     </el-checkbox-group>
+    <el-tree :data="data" :props="defaultProps" accordion @node-click="handleNodeClick">
+    </el-tree>
   </div>
 </template>
 
@@ -33,8 +35,47 @@ export default {
       checkboxGroup1: ['上海'],
       checkboxGroup2: ['北京'],
       checkboxGroup3: ['广州'],
-      cities: cityOptions
-    }
+      cities: cityOptions,
+      data: [{
+        label: '一级 1',
+        children: [{
+          label: '二级 1-1',
+          children: [{
+            label: '三级 1-1-1'
+          }]
+        }]
+      }, {
+        label: '一级 2',
+        children: [{
+          label: '二级 2-1',
+          children: [{
+            label: '三级 2-1-1'
+          }]
+        }, {
+          label: '二级 2-2',
+          children: [{
+            label: '三级 2-2-1'
+          }]
+        }]
+      }, {
+        label: '一级 3',
+        children: [{
+          label: '二级 3-1',
+          children: [{
+            label: '三级 3-1-1'
+          }]
+        }, {
+          label: '二级 3-2',
+          children: [{
+            label: '三级 3-2-1'
+          }]
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    };
   },
 
   methods: {

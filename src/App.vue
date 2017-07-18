4b6239b059334e752e9f7214adfe8c2cd5490820
Bob-huang-gdut <3570411064@qq.com>
2017-07-18 19:04:50 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,25 @@
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <el-button @click.native="startHacking">Let's do it</el-button>
+    <el-switch v-model="value1" on-text="" off-text="">
+    </el-switch>
+    <el-switch v-model="value2" on-color="#13ce66" off-color="#ff4949">
+    </el-switch>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      msg: 'Use Vue 2.0 Today!'
+      msg: 'Use Vue 2.0 Today!',
+      value1: true,
+      value2: true
     }
   },
 
   methods: {
-    startHacking () {
+    startHacking() {
       this.$notify({
         title: 'It Works',
         message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
+import '../theme/index.css'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
 
 Vue.use(ElementUI)

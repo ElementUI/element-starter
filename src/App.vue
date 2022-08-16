d2c5f540a52a6517d2d46de0b15ed57cc62ae976
YaoLi3 <liyaoo6o@gmail.com>
2022-08-16 17:02:13 +0800

    init web

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,62 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <el-button @click="startHacking">Start</el-button>
-    </div>
+    <header>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" active-text-color="#409eff" style="margin: 0px;">
+        <el-menu-item index="1">Home</el-menu-item>
+        <el-menu-item index="2">Cluster</el-menu-item>
+        <el-menu-item index="3" disabled>About Us</a></el-menu-item>
+      </el-menu>
+      <div class="line"></div>
+    </header>
+    <article>
+        <component v-bind:is="selected"></component>
+    </article>
+    <hr>
+    <footer>
+      <div class="line"></div>
+      <p>Contact: guolidong@genomics.cn; liyao1@genomics.cn; wangrui</p>
+      <p>Single Cell</p>
+    </footer>
   </div>
 </template>
 
 <script>
+
+import Home from "./Home.vue";
+import Cluster from './Cluster.vue'
+
+
 export default {
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
+  data() {
+    return {
+      activeIndex: '1',
+      selected:"Home"
     }
-  }
+  },
+  components:{
+     Home,
+     Cluster
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+        if ( key == "2" )
+        {
+            this.selected = "Cluster";
+        }
+        else if ( key == "1")
+        {
+            this.selected = "Home";
+        }
+        else if ( key == "3")
+        {
+            this.selected = "About Us";
+        }
+        else
+        {
+            this.selected = "Home";
+        }
+    }
+  },
 }
 </script>
 
@@ -26,5 +64,12 @@ export default {
 #app {
   font-family: Helvetica, sans-serif;
   text-align: center;
+  margin-top: -8px;
 }
+header{
+    position: sticky;
+    z-index: 999;
+    top: 0;
+    margin-bottom: 8px;
+    }
 </style>
---
+++ src/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>Single Cell White Paper by BGI-Qingdao</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
